0dabffa7fec2a218b5c5ab2282df4b7d5b1d9dcd
Quarazy <quarazy@gmail.com>
2016-08-07 12:38:11 -0700

    Simple json test on message

--- message.go
+++ message.go
@@ -12,8 +12,15 @@ const (
 type Message struct {
 	Type MessageType `json:"type"`
 	Id   uint64      `json:"id"`
+	Msec float64     `json:"msec"`
 	X    float32     `json:"x"`
 	Y    float32     `json:"y"`
 	Z    float32     `json:"z"`
 	Step uint16      `json:"step"`
+}
+
+// Half-Life style user input
+type UserCommand struct {
+	lerpMsec uint16
+	msec     byte
 }
---
+++ message_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const Epsilon = 1e-6
+
+var _ = Describe("Message", func() {
+	var message *Message
+	var byt []byte
+
+	BeforeEach(func() {
+		message = &Message{}
+		byt = []byte(`{"type":1,"id":13,"msec":16.66,"x":1.3,"y":-2.2,"z":0,"step":56}`)
+	})
+
+	It("Serializes", func() {
+		if err := json.Unmarshal(byt, message); err != nil {
+			Fail(fmt.Sprintf("Error when unmarshalling: %s", err))
+		}
+		Expect(message.Type).To(Equal(User))
+		Expect(message.X).To(BeNumerically("~", 1.3, Epsilon))
+		Expect(message.Y).To(BeNumerically("~", -2.2, Epsilon))
+		Expect(message.Z).To(BeNumerically("==", 0))
+		Expect(message.Step).To(BeNumerically("==", 56))
+	})
+
+	It("Deserializes", func() {
+		message = &Message{
+			Type: 1,
+			Id:   13,
+			Msec: 16.66,
+			X:    1.3,
+			Y:    -2.2,
+			Z:    0,
+			Step: 56,
+		}
+		des, _ := json.Marshal(message)
+		Expect(des).To(Equal(byt))
+	})
+})
